0af348eec70a020eaf80ed6a4d8f9af790d4407d
kerimoff-0548 <kmagomed2000@mail.ru>
2024-12-18 11:00:24 +0300

    dada2

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   return (
   <Router>
     <Routes>
-      <Route path="/" element={<TodoPage />} />
+      <Route path="/rkpo" element={<TodoPage />} />
       <Route path="/dnd" element={<DndPage />} />
     </Routes>
   </Router>
--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "https://kerico-0548.github.io/rkpo",
   build: {
     outDir: "build"
   },
